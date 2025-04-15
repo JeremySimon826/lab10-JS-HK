164e7220e43de9a1ff245e5551e8ce46ef3eda5f
jeremy <jeremy.michael.simon@gmail.com>
2025-04-14 22:58:29 -0400

    merged changes

--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,24 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
 
+
+    def test_add(self):
+        self.assertEqual(add(3, 4), 7)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(0, 10)
+
+    def test_logarithm(self):
+        self.assertAlmostEqual(logarithm(10, 1000), 3.0)
+
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            logarithm(1, 10)
+
     def test_multiply(self):
         self.assertEqual(multiply(3, 4), 12)
         self.assertEqual(multiply(-1, 5), -5)
